6ef49634d55ae532041798cf0b97079485e1ba6c
Vedantrwt-hub <vedantrawat115@gmail.com>
2026-09-20 00:56:38 +0530

    Fix production CORS

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,40 +1,33 @@
 const express = require("express");
-const cookieparser = require("cookie-parser");
+const cookieParser = require("cookie-parser");
 const cors = require("cors");
 
 const app = express();
 
+app.use(express.json());
+app.use(cookieParser());
+
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://mern-frontend-7.vercel.app",
+  "https://mern-frontend-sd94.vercel.app",
 ];
 
 app.use(
   cors({
     origin: function (origin, callback) {
-      // Allow requests like Postman/server-to-server
-      if (!origin) {
-        return callback(null, true);
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error(`CORS blocked for origin: ${origin}`));
       }
-
-      if (allowedOrigins.includes(origin)) {
-        return callback(null, true);
-      }
-
-      return callback(new Error("Not allowed by CORS"));
     },
     credentials: true,
   })
 );
 
-app.use(express.json());
-app.use(cookieparser());
-
-/* require all the router here */
 const authRouter = require("./Router/auth.routes");
 const interviewRouter = require("./Router/interview.routes");
 
-/* using all the router here */
 app.use("/api/auth", authRouter);
 app.use("/api/interview", interviewRouter);
 
