cf574ffac5f3631a7222a6a921a17d77e1d86d65
Vedantrwt-hub <vedantrawat115@gmail.com>
2026-09-20 00:47:10 +0530

    Fix production authentication cookies

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -41,7 +41,12 @@ if(userAlreadyExists){
     {expiresIn:"1d"}
   )
 
-  res.cookie("token",token)
+  res.cookie("token", token, {
+  httpOnly: true,
+  secure: true,
+  sameSite: "none",
+  maxAge: 24 * 60 * 60 * 1000
+});
 
   res.status(201).json({
     massage :"User successfully registered",
@@ -98,7 +103,12 @@ async function loginUserController(req,res) {
        {expiresIn:"1d"}
   )
 
-  res.cookie("token",token)
+  res.cookie("token", token, {
+  httpOnly: true,
+  secure: true,
+  sameSite: "none",
+  maxAge: 24 * 60 * 60 * 1000
+}); 
   res.status(200).json({
     message:"User LoggedIn Successfully",
     user:{
